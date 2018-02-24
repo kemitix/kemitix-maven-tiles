7fbdee9d70e9555fb0b26cbf81f40d990f93f672
Paul Campbell <pcampbell@kemitix.net>
2018-02-24 20:24:42 +0000

    Improve version setting

--- set-version.sh
+++ set-version.sh
@@ -12,4 +12,5 @@ NEXT=$2
 echo Updating version from $CURRENT to $NEXT
 
 ./mvnw versions:set -DnewVersion=$NEXT
+./mvnw -pl parent versions:set -DnewVersion=$NEXT
 perl -p -i -e "s,$CURRENT</,$NEXT</," all/tile.xml README.md
