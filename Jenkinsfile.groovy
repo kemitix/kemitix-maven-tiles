97231cf61a9b70276fdbf10a8b400a766da0c409
Paul Campbell <pcampbell@kemitix.net>
2018-07-07 12:01:56 +0100

    jenkins: simplify and only deploy from published github release branch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,20 +1,9 @@
+final String publicRepo = 'https://github.com/kemitix/'
 final String mvn = "mvn --batch-mode --update-snapshots --errors"
 
 pipeline {
     agent any
     stages {
-        stage('release != SNAPSHOT') {
-            // checks that the pom version is not a SNAPSHOT when the current branch is a release
-            when {
-                expression {
-                    (branchStartsWith('release/')) &&
-                            (readMavenPom(file: 'pom.xml').version).contains("SNAPSHOT")
-                }
-            }
-            steps {
-                error("Build failed because SNAPSHOT version: [" + env.GIT_BRANCH + "]")
-            }
-        }
         stage('Install') {
             steps {
                 withMaven(maven: 'maven', jdk: 'JDK 1.8') {
@@ -22,29 +11,39 @@ pipeline {
                 }
             }
         }
-        stage('Deploy (release on gitlab)') {
+        stage('Deploy (published release branch)') {
             when {
                 expression {
-                    (branchStartsWith('release/') && isPublished())
+                    (isReleaseBranch() &&
+                            isPublished(publicRepo) &&
+                            notSnapshot())
                 }
             }
             steps {
                 withMaven(maven: 'maven', jdk: 'JDK 1.8') {
-                    sh "${mvn} deploy --activate-profiles release -DskipTests=true -Dpitest.skip=true"
+                    sh "${mvn} --activate-profiles release deploy"
                 }
             }
         }
     }
 }
 
-private boolean branchStartsWith(String branchName) {
+private boolean isReleaseBranch() {
+    return branchStartsWith('release/')
+}
+
+private boolean branchStartsWith(final String branchName) {
     startsWith(env.GIT_BRANCH, branchName)
 }
 
-private boolean isPublished() {
-    startsWith(env.GIT_URL, 'https://')
+private boolean isPublished(final String repo) {
+    startsWith(env.GIT_URL, repo)
 }
 
-private boolean startsWith(String value, String match) {
+private static boolean startsWith(final String value, final String match) {
     value != null && value.startsWith(match)
+}
+
+private boolean notSnapshot() {
+    return !(readMavenPom(file: 'pom.xml').version).contains("SNAPSHOT")
 }
