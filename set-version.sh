eacddc5fa5465f7a42547b9832edba216c16bb63
Paul Campbell <pcampbell@kemitix.net>
2018-08-28 22:54:20 +0100

    Don't use removed maven wrapper in set-version.sh

--- set-version.sh
+++ set-version.sh
@@ -14,6 +14,6 @@ NEXT=$1
 
 echo Updating version from $CURRENT to $NEXT
 
-./mvnw versions:set -DnewVersion=$NEXT
-./mvnw -pl parent versions:set -DnewVersion=$NEXT
-perl -p -i -e "s,$CURRENT</,$NEXT</," all/tile.xml README.md
+mvn versions:set -DnewVersion=$NEXT
+mvn -pl parent versions:set -DnewVersion=$NEXT
+perl -p -i -e "s,$CURRENT</,$NEXT</," README.md
