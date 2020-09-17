70cfae8ddb47e5d7558b52e9e1f56d9044155b59
Paul Campbell <pcampbell@kemitix.net>
2020-09-17 07:49:52 +0100

    update set-version to remove README.org

--- set-version.sh
+++ set-version.sh
@@ -16,4 +16,3 @@ echo Updating version from $CURRENT to $NEXT
 
 mvn versions:set -DnewVersion=$NEXT
 mvn -pl parent versions:set -DnewVersion=$NEXT
-perl -p -i -e "s,$CURRENT</,$NEXT</," README.org
